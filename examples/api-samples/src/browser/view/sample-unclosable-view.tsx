c24e9f8eb3f1200e90f6870fc396edbc7dc03fef
Akos Kitta <kittaakos@typefox.io>
2021-02-17 11:34:04 +0100

    added an example
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/view/sample-unclosable-view.tsx
+++ examples/api-samples/src/browser/view/sample-unclosable-view.tsx
@@ -15,7 +15,8 @@
  ********************************************************************************/
 
 import { ReactWidget } from '@theia/core/lib/browser';
-import { injectable, postConstruct } from 'inversify';
+import { inject, injectable, postConstruct } from 'inversify';
+import { CommandService } from '@theia/core/lib/common/command';
 import * as React from 'react';
 
 /**
@@ -24,6 +25,9 @@ import * as React from 'react';
 @injectable()
 export class SampleViewUnclosableView extends ReactWidget {
   static readonly ID = 'sampleUnclosableView';
+
+  @inject(CommandService)
+  protected readonly commandService: CommandService;
 
   @postConstruct()
   init(): void {
@@ -40,7 +44,74 @@ export class SampleViewUnclosableView extends ReactWidget {
       <div>
         Closable
         <input type="checkbox" defaultChecked={this.title.closable} onChange={e => this.title.closable = e.target.checked} />
+        <TheiaSwizardExtensionView commandService={this.commandService} />
       </div>
     );
   }
+}
+
+interface Sensor {
+  brand: string;
+  model: string;
+  name: string;
+}
+
+export class SensorView extends React.Component<Sensor> {
+  render(): JSX.Element {
+    return <React.Fragment>
+      <ul>
+        <li>Brand: {this.props.brand}</li>
+        <li>Model: {this.props.model}</li>
+        <li>Name: {this.props.name}</li>
+      </ul>
+    </React.Fragment>;
+  }
+}
+
+export class TheiaSwizardExtensionView extends React.Component<{ commandService: CommandService }, Sensor> {
+
+  constructor(props: { commandService: CommandService }) {
+    super(props);
+
+    this.state = {
+      brand: '',
+      model: '',
+      name: ''
+    };
+  }
+
+  render(): JSX.Element {
+    return <div id="widget-container">
+
+      <h2>New Sensor</h2>
+
+      <SensorView brand={this.state.brand} model={this.state.model} name={this.state.name} />
+
+      <form onSubmit={this.handleSubmit}>
+        <label>Brand: <input value={this.state.brand} onChange={this.updateBrand} /></label>
+        <label>Model: <input value={this.state.model} onChange={this.updateModel} /></label>
+        <input type="submit" value="OK" />
+      </form>
+    </div>;
+  }
+
+  protected handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.getName();
+    console.log(typeof this.props.commandService, `Here you can invoke the command service with the state ${JSON.stringify(this.state)}`);
+  };
+
+  protected updateBrand = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({
+    brand: e.currentTarget.value
+  });
+
+  protected updateModel = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({
+    model: e.currentTarget.value
+  });
+
+  protected getName = () => this.setState({
+    name: `${this.cleanString(this.state.brand)}_${this.cleanString(this.state.model)}`
+  });
+
+  protected cleanString = (s: string) => s.toUpperCase().replace(/ /g, '_');
 }
