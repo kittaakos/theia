a9f4e2e1686db1d70c3db323967a7096bb359110
Akos Kitta <kittaakos@typefox.io>
2021-03-02 11:14:16 +0100

    Customized the workspace preferences in final app.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -14,21 +14,36 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { ContainerModule } from 'inversify';
+import { injectable, ContainerModule } from 'inversify';
 import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-command-contribution';
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
 import { bindSampleOutputChannelWithSeverity } from './output/sample-output-channel-with-severity';
 import { bindSampleMenu } from './menu/sample-menu-contribution';
 import { bindSampleFileWatching } from './file-watching/sample-file-watching-contribution';
 import { bindVSXCommand } from './vsx/sample-vsx-command-contribution';
+import { PreferenceSchema } from '@theia/core/lib/common/preferences/preference-schema';
+import { workspacePreferenceSchema, WorkspacePreferencesSchemaProvider } from '@theia/workspace/lib/browser/workspace-preferences';
 
 import '../../src/browser/style/branding.css';
 
-export default new ContainerModule(bind => {
+export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
     bindSampleOutputChannelWithSeverity(bind);
     bindSampleMenu(bind);
     bindSampleFileWatching(bind);
     bindVSXCommand(bind);
+    bind(MyWorkspacePreferencesSchemaProvider).toSelf().inSingletonScope();
+    rebind(WorkspacePreferencesSchemaProvider).toService(MyWorkspacePreferencesSchemaProvider);
 });
+
+@injectable()
+class MyWorkspacePreferencesSchemaProvider extends WorkspacePreferencesSchemaProvider {
+
+    get schema(): PreferenceSchema {
+        const schema = workspacePreferenceSchema;
+        delete schema.properties['workspace.preserveWindow'];
+        return schema;
+    }
+
+}
