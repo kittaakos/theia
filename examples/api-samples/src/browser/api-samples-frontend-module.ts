462376a7993c9b14d4ab963296516d7955803c36
Akos Kitta <a.kitta@arduino.cc>
2022-06-17 15:33:16 +0200

    added example.
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -14,7 +14,7 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { ContainerModule, interfaces } from '@theia/core/shared/inversify';
+import { ContainerModule, inject, injectable, interfaces } from '@theia/core/shared/inversify';
 import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-command-contribution';
 import { bindSampleFilteredCommandContribution } from './contribution-filter/sample-filtered-command-contribution';
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
@@ -26,6 +26,9 @@ import { bindSampleToolbarContribution } from './toolbar/sample-toolbar-contribu
 
 import '../../src/browser/style/branding.css';
 import { bindMonacoPreferenceExtractor } from './monaco-editor-preferences/monaco-editor-preference-extractor';
+import { FrontendApplicationContribution } from '@theia/core/lib/browser';
+import { EditorManager, EditorOpenerOptions } from '@theia/editor/lib/browser';
+import { wait } from '@theia/core/lib/common/promise-util';
 
 export default new ContainerModule((
     bind: interfaces.Bind,
@@ -42,4 +45,35 @@ export default new ContainerModule((
     bindSampleFilteredCommandContribution(bind);
     bindSampleToolbarContribution(bind, rebind);
     bindMonacoPreferenceExtractor(bind);
+    bind(EditorOpenerOptionsContribution).toSelf().inSingletonScope();
+    bind(FrontendApplicationContribution).toService(EditorOpenerOptionsContribution);
 });
+
+@injectable()
+class EditorOpenerOptionsContribution implements FrontendApplicationContribution {
+
+    @inject(EditorManager)
+    private readonly editorManager: EditorManager;
+
+    onStart(): void {
+        this.editorManager.onCreated(async widget => {
+            await wait(2000);
+            const options = {
+                counter: 0,
+                mode: 'reveal',
+                preview: false
+            } as EditorOpenerOptions;
+            console.log(`#1 Opening editor ${widget.editor.uri.toString()} with options: ${JSON.stringify(options)}`);
+            await this.editorManager.open(widget.editor.uri, options);
+            await wait(2000);
+            const sameOptions = {
+                ...options,
+                preview: options.preview,
+                mode: options.mode
+            };
+            console.log(`#2 Opening editor ${widget.editor.uri.toString()} with options: ${JSON.stringify(sameOptions)}`);
+            await this.editorManager.open(widget.editor.uri, sameOptions);
+        });
+    }
+
+}
